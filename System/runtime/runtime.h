a03f97a1d17475c81f9222b7f2715721639126b3
8BIT0 <971168718@qq.com>
2022-02-08 18:11:39 +0800

    runtime update

--- System/runtime/runtime.h
+++ System/runtime/runtime.h
@@ -7,26 +7,23 @@
 #define REAL_MS 1000 /* 1ms == 1000us */
 #define REAL_S REAL_MS * 1000
 
-#define RUNTIEM_MAX_TICK_FRQ RUNTIME_TICK_FRQ_1M
+#define RUNTIEM_MAX_TICK_FRQ RUNTIME_TICK_FRQ_20K
 
-#define RUNTIME_TICK_FRQ_1M 1e6    /* 1us base time */
-#define RUNTIME_TICK_FRQ_500K 5e5  /* 2us base time */
-#define RUNTIMT_TICK_FRQ_250K 25e4 /* 4us bse time */
-#define RUNTIME_TICK_FRQ_200K 20e4 /* 5us base time */
-#define RUNTIME_TICK_FRQ_100K 10e4 /* 10us base time */
-#define RUNTIME_TICK_FRQ_40K 4e4   /* 25us base time */
-#define RUNTIME_TICK_FRQ_50K 5e4   /* 20us base time */
-#define RUNTIME_TICK_FRQ_25K 25e3  /* 40us base time */
-#define RUNTIME_TICK_FRQ_20K 2e4   /* 50us base time */
-#define RUNTIME_TICK_FRQ_10K 1e4   /* 100us base time */
-#define RUNTIME_TICK_FRQ_5K 5e3    /* 200us base time */
-#define RUNTIME_TICK_FRQ_2K 2e3    /* 500us base time */
-#define RUNTIME_TICK_FRQ_1K 1e3    /* 1ms base time */
+#define RUNTIME_TICK_FRQ_20K 2e4 /* 50us base time */
+#define RUNTIME_TICK_FRQ_10K 1e4 /* 100us base time */
+#define RUNTIME_TICK_FRQ_5K 5e3  /* 200us base time */
+#define RUNTIME_TICK_FRQ_2K 2e3  /* 500us base time */
+#define RUNTIME_TICK_FRQ_1K 1e3  /* 1ms base time */
+
+typedef enum
+{
+    RtCallback_Type_Start = 0,
+    RtCallback_Type_Stop,
+    RtCallback_Type_Tick,
+} Runtime_BaseCallback_TypeList;
 
 typedef bool (*runtime_stop_p)(void);
-typedef uint32_t (*runtime_start_callback_p)(void);
-typedef uint32_t (*runtime_stop_callback_p)(void);
-typedef uint32_t (*runtime_tick_callback_p)(void);
+typedef uint32_t (*runtime_callback_p)(void);
 
 typedef uint64_t SYSTEM_RunTime;
 typedef uint32_t Tick_Frq;
@@ -53,17 +50,15 @@ typedef struct
     Tick_Base base;
     Tick_Frq frq;
 
-    runtime_start_callback_p start_callback;
-    runtime_stop_callback_p stop_callback;
-    runtime_tick_callback_p tick_callback;
+    runtime_callback_p start_callback;
+    runtime_callback_p stop_callback;
+    runtime_callback_p tick_callback;
 
     Runtime_ModuleState_List module_state;
     Runtime_RunState_List tick_state;
 } Runtime_DataObj_TypeDef;
 
-void Runtime_Set_stop_Callback(runtime_stop_callback_p stop_cb);
-void Runtime_Set_tick_Callback(runtime_tick_callback_p tick_cb);
-void Runtime_Set_start_Callback(runtime_start_callback_p start_cb);
+bool Runtime_SetCallback(Runtime_BaseCallback_TypeList type, runtime_callback_p stop_cb);
 void RuntimeObj_Reset(SYSTEM_RunTime *Obj);
 
 bool Runtime_Config(uint32_t tick_frq);
