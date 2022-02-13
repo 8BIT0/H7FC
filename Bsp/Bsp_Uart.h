be385d53718167c88713972a5b83983fe0f1e592
8BIT0 <971168718@qq.com>
2022-02-13 21:23:14 +0800

    Bsp_Uart update

--- Bsp/Bsp_Uart.h
+++ Bsp/Bsp_Uart.h
@@ -5,17 +5,31 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef void (*UartTx_Callback)(uint8_t *tx, uint16_t size);
+typedef void (*UartRx_Callback)(uint8_t *rx, uint16_t size);
+
 #pragma pack(1)
 typedef struct
 {
     uint8_t id;
+
+    UartTx_Callback TxCallback;
+    UartRx_Callback RxCallback;
+
+    uint8_t *TxBuff;
+    uint8_t *RxBuff;
+
+    uint16_t TxSize;
+    uint16_t RxSize;
 } BspUARTObj_TypeDef;
 #pragma pack()
 
 typedef struct
 {
     bool (*init)(BspUARTObj_TypeDef *obj);
+    bool (*de_init)(BspUARTObj_TypeDef *obj);
     bool (*write)(BspUARTObj_TypeDef *obj, uint8_t *tx, uint32_t size);
+    uint16_t (*get_RecCount)(BspUARTObj_TypeDef *obj);
 } BspUART_TypeDef;
 
 #endif
