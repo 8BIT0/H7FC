bbefd59679cb994be6877478a46dac72f146902c
8BIT0 <971168718@qq.com>
2022-02-13 21:12:49 +0800

    Bsp_Uart update

--- Bsp/Bsp_Uart.h
+++ Bsp/Bsp_Uart.h
@@ -5,4 +5,17 @@
 #include <stdbool.h>
 #include <string.h>
 
+#pragma pack(1)
+typedef struct
+{
+    uint8_t id;
+} BspUARTObj_TypeDef;
+#pragma pack()
+
+typedef struct
+{
+    bool (*init)(BspUARTObj_TypeDef *obj);
+    bool (*write)(BspUARTObj_TypeDef *obj, uint8_t *tx, uint32_t size);
+} BspUART_TypeDef;
+
 #endif
