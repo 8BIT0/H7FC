fa13805a38be62dfeb1dd13dddaa0b5bcd633b98
8BIT0 <971168718@qq.com>
2022-02-17 22:59:54 +0800

    storage update

--- System/storage/storage.c
+++ System/storage/storage.c
@@ -1,10 +1,35 @@
 #include "storage.h"
 #include "Dev_W25Qxx.h"
 
+#if (EXTERNAL_STORAGE_MODULE == 1)
+DevW25QxxObj_TypeDef W25Q64_Obj;
+
+bool ExtStorage_Init(void)
+{
+    bool init_state = false;
+
+    DevW25Q64.init(W25Q64_Obj);
+
+    return init_state;
+}
+#endif
+
+bool IntStorage_Init(void)
+{
+    bool init_state = false;
+
+    return init_state;
+}
+
 bool Storage_Init(void)
 {
 #if (EXTERNAL_STORAGE_MODULE == 1)
-    bool ExModule_Init = false;
-
+    if (!ExtStorage_Init())
+        return false;
 #endif
+
+    if (!IntStorage_Init())
+        return false;
+
+    return true;
 }
