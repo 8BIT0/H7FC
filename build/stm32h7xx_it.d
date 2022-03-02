4f14ec5e60026af36e2a52f3c4a6986a8d793edb
8BIT0 <971168718@qq.com>
2022-03-02 22:27:15 +0800

    kernel update

--- build/stm32h7xx_it.d
+++
@@ -1,6 +0,0 @@
-build/stm32h7xx_it.o: HAL_Lib/Core/Src/stm32h7xx_it.c \
- HAL_Lib/Core/Inc/stm32h7xx_it.h System/runtime/runtime.h \
- System/system_cfg.h
-HAL_Lib/Core/Inc/stm32h7xx_it.h:
-System/runtime/runtime.h:
-System/system_cfg.h:
