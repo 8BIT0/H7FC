b11a3977c01b31d25c766420e1e32f1052469d64
8BIT0 <971168718@qq.com>
2022-03-22 23:25:42 +0800

    SrvIMU update

--- Service/SrvMPU_Sample.c
+++ Service/SrvMPU_Sample.c
@@ -1,10 +1,12 @@
 #include "SrvMPU_Sample.h"
 #include "Bsp_SPI.h"
+#include "Bsp_GPIO.h"
 #include "Dev_MPU6000.h"
 
-static SPI_HandleTypeDef MPU6000_SPI;
-static SPI_HandleTypeDef ICM20602_SPI;
+static SPI_HandleTypeDef MPU6000_SPI_Instance;
+static BspGPIO_Obj_TypeDef MPU6000_CSPin_Cfg;
+static BspSPI_NorModeConfig_TypeDef MPU6000_SPI_Cfg;
 
-static bool SrvIMU_Init(void)
-{
-}
+static SPI_HandleTypeDef ICM20602_SPI_Instance;
+static BspSPI_NorModeConfig_TypeDef ICM20602_SPI_Cfg;
+static BspGPIO_Obj_TypeDef ICM20602_CSPin_Cfg;
