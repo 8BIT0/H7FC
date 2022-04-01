b08d0fef185c053fb0791a574d3b14bacc38126e
8BIT0 <971168718@qq.com>
2022-04-01 23:11:21 +0800

    Dev_ICM20602 update

--- Device/Dev_ICM20602.c
+++ Device/Dev_ICM20602.c
@@ -9,20 +9,23 @@ static bool DevICM20602_PreInit()
 {
 }
 
-static bool DevICM20602_Init()
+static ICM20602_Error_List DevICM20602_Init(DevICM20602Obj_TypeDef *Obj)
 {
+    return ICM20602_No_Error;
 }
 
-static void DevICM20602_SetDRDY()
+static void DevICM20602_SetDRDY(DevICM20602Obj_TypeDef *Obj)
 {
+    Obj->drdy = true;
 }
 
 static bool DevICM20602_SwReset()
 {
 }
 
-static bool DevICM20602_GetReady()
+static bool DevICM20602_GetReady(DevICM20602Obj_TypeDef *Obj)
 {
+    return Obj->drdy;
 }
 
 static void DevICM20602_Sample()
