af6a277dfd1335fe61d5fa0e76658f429e418cfc
8BIT0 <971168718@qq.com>
2022-04-06 14:52:16 +0800

    update

--- Task/Task_Manager.c
+++ Task/Task_Manager.c
@@ -56,9 +56,9 @@ void Task_Manager_Init(void)
 void Task_Manager_CreateTask(void)
 {
     TaskInertial_Handle = Os_CreateTask("Inertial Sample", TASK_EXEC_4KHZ, Task_Group_0, Task_Group_0, TaskInertical_Core, 1024);
+    TaskProtocol_Handle = Os_CreateTask("Protocl", TASK_EXEC_20HZ, Task_Group_1, Task_Priority_0, TaskProtocol_Core, 1024);
     Test2_Task = Os_CreateTask("test2", TASK_EXEC_1KHZ, Task_Group_0, Task_Group_2, Test2, 256);
     Test_Task = Os_CreateTask("test delay", TASK_EXEC_8KHZ, Task_Group_0, Task_Group_1, Test, 256);
-    TaskProtocol_Handle = Os_CreateTask("Protocl", TASK_EXEC_20HZ, Task_Group_1, Task_Priority_0, TaskProtocol_Core, 1024);
 }
 
 void Test2(Task_Handle handle)
