493c0cb341dc4af1ba4999a377322c5c66c25c3b
8BIT0 <971168718@qq.com>
2022-05-23 22:37:08 +0800

    queue update

--- build/queue.d
+++
@@ -1,4 +0,0 @@
-build/queue.o: DataStructure/queue.c DataStructure/queue.h \
- System/mmu/mmu.h
-DataStructure/queue.h:
-System/mmu/mmu.h:
--- build/shell_port.d
+++
@@ -1,5 +0,0 @@
-build/shell_port.o: System/shell/shell_port.c System/shell/shell.h \
- System/shell/shell_cfg.h System/shell/shell_port.h
-System/shell/shell.h:
-System/shell/shell_cfg.h:
-System/shell/shell_port.h:
