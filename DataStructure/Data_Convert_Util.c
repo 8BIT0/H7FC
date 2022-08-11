d77e007247705e7bb73f1fc590097a9adb457f6b
8BIT0 <971168718@qq.com>
2022-08-11 11:44:24 +0800

    Data_Convert_Util update

--- DataStructure/Data_Convert_Util.c
+++ DataStructure/Data_Convert_Util.c
@@ -24,6 +24,32 @@ uint16_t LEndian2HalfWord(const uint8_t *ptr)
     return tmp;
 }
 
+bool LEndianWord2BytesArray(const uint32_t target, uint8_t *p_bytes)
+{
+    if (p_bytes)
+    {
+        p_bytes[0] = target & 0x000000FF;
+        p_bytes[1] = (target & 0x0000FF00) >> 8;
+        p_bytes[2] = (target & 0x00FF0000) >> 16;
+        p_bytes[3] = (target & 0xFF000000) >> 24;
+        return true;
+    }
+
+    return false;
+}
+
+bool LEndianHalfWord2BytesArray(const uint16_t target, uint8_t *p_bytes)
+{
+    if (p_bytes)
+    {
+        p_bytes[0] = target & 0x00FF;
+        p_bytes[1] = (target & 0xFF00) >> 8;
+        return true;
+    }
+
+    return false;
+}
+
 /******************************************************* Big Endain Convert ****************************************************************/
 
 uint32_t BEndian2Word(const uint8_t *ptr)
--- DataStructure/Data_Convert_Util.h
+++ DataStructure/Data_Convert_Util.h
@@ -5,6 +5,8 @@
 
 uint32_t LEndian2Word(const uint8_t *ptr);
 uint16_t LEndian2HalfWord(const uint8_t *ptr);
+bool LEndianHalfWord2BytesArray(const uint16_t target, uint8_t *p_bytes);
+bool LEndianWord2BytesArray(const uint32_t target, uint8_t *p_bytes);
 
 uint32_t BEndian2Word(const uint8_t *ptr);
 uint16_t BEndian2HalfWord(const uint8_t *ptr);
