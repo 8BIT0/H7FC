46f43d3720444a041170778e85420b96b12f7617
8BIT0 <971168718@qq.com>
2022-08-31 10:32:27 +0800

    DiskIO update

--- System/diskio/DiskIO.c
+++ System/diskio/DiskIO.c
@@ -1463,12 +1463,9 @@ static bool Disk_WriteFile_From_Head(Disk_FATFileSys_TypeDef *FATObj, Disk_FileO
     FileObj->info.size += len;
 
     /* update file size */
-    DevCard.read(&DevTFCard_Obj.SDMMC_Obj, FileObj.start_sec, Disk_Card_SectionBuff, DISK_CARD_SECTION_SZIE, 1);
-    ((Disk_CCSSFFAT_TypeDef *)Disk_Card_SectionBuff)->attribute[FileObj->info_index].FileSize[0] = ;
-    ((Disk_CCSSFFAT_TypeDef *)Disk_Card_SectionBuff)->attribute[FileObj->info_index].FileSize[1] = ;
-    ((Disk_CCSSFFAT_TypeDef *)Disk_Card_SectionBuff)->attribute[FileObj->info_index].FileSize[2] = ;
-    ((Disk_CCSSFFAT_TypeDef *)Disk_Card_SectionBuff)->attribute[FileObj->info_index].FileSize[3] = ;
-    DevCard.write(&DevTFCard_Obj.SDMMC_Obj, FileObj.start_sec, Disk_Card_SectionBuff, DISK_CARD_SECTION_SZIE, 1);
+    DevCard.read(&DevTFCard_Obj.SDMMC_Obj, FileObj->start_sec, Disk_Card_SectionBuff, DISK_CARD_SECTION_SZIE, 1);
+    LEndianWord2BytesArray(((Disk_CCSSFFAT_TypeDef *)Disk_Card_SectionBuff)->attribute[FileObj->info_index].FileSize, FileObj->info.size);
+    DevCard.write(&DevTFCard_Obj.SDMMC_Obj, FileObj->start_sec, Disk_Card_SectionBuff, DISK_CARD_SECTION_SZIE, 1);
 
     return true;
 }
