c1adc0a92b95f559a31c756a403790544d73d8a4
8BIT0 <971168718@qq.com>
2022-09-03 15:14:16 +0800

    DiskIO updare

--- Device/Dev_Card.c
+++ Device/Dev_Card.c
@@ -75,7 +75,7 @@ static DevCard_Info_TypeDef DevCard_GetInfo(DevCard_Obj_TypeDef *Instance)
 
 static bool DevCard_Write(DevCard_Obj_TypeDef *Instance, uint32_t block, uint8_t *p_data, uint16_t data_size, uint16_t block_num)
 {
-    if ((Instance == NULL) || (p_data == NULL) || (block_num == 0) || (block == 0) || (block > Instance->info.BlockNbr) || (data_size != block_num * Instance->info.BlockSize))
+    if ((Instance == NULL) || (p_data == NULL) || (block_num == 0) || (block == 0) || (block > Instance->info.BlockNbr))
         return false;
 
     BspSDMMC.write(&(Instance->SDMMC_Obj), p_data, block, block_num);
