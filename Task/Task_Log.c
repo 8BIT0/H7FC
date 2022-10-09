fa94c23d131ff943578067227bc58e803fe93bb2
‘8BIT0’ <971168718@qq.com>
2022-10-09 22:26:59 +0800

    TaskLog update

--- Task/Task_Log.c
+++ Task/Task_Log.c
@@ -24,8 +24,10 @@
 #define LOG_FOLDER "log/"
 #define IMU_LOG_FILE "imu.log"
 
-#define M_BYTE 1024 * 1024
-#define MAX_FILE_SIZE_M(x) x *M_BYTE
+#define K_BYTE 1024
+#define M_BYTE K_BYTE * K_BYTE
+#define MAX_FILE_SIZE_M(x) x * M_BYTE
+#define MAX_FILE_SIZE_K(x) x * K_BYTE
 #define MIN_CACHE_NUM 2
 
 /* internal variable */
@@ -65,8 +67,8 @@ void TaskLog_Init(void)
         LogFile_Obj = Disk.create_file(&FATFS_Obj, IMU_LOG_FILE, LogFolder_Cluster);
         Disk.open(&FATFS_Obj, LOG_FOLDER, IMU_LOG_FILE, &LogFile_Obj);
 
-        /* create cache */
-        if (TaskLog_CreateCache(&IMU_LogMonitor, MIN_CACHE_NUM, FATFS_Obj.BytePerSection, LogIMU_Header))
+        /* create cache 3K for IMU Data */
+        if (TaskLog_CreateCache(&IMU_LogMonitor, 6, FATFS_Obj.BytePerSection, LogIMU_Header))
             LogFile_Ready = true;
     }
 }
@@ -93,7 +95,7 @@ void TaskLog_Core(Task_Handle hdl)
             DevLED.ctl(Led2, led_state);
         }
 
-        if (LogFile_Obj.info.size < MAX_FILE_SIZE_M(1))
+        if (LogFile_Obj.info.size < MAX_FILE_SIZE_M(8))
         {
             TaskLog_IMU_ToFile(&IMU_LogMonitor);
         }
@@ -114,7 +116,7 @@ static bool TaskLog_CreateCache(Log_Monitor_TypeDef *obj, uint8_t cache_sum, uin
     if ((obj == NULL) || (cache_sum <= 1) || (cache_buff_size == 0))
         return false;
 
-    obj->cache_obj = (LogCache_TypeDef *)MMU_Malloc(sizeof(LogCache_TypeDef) * cache_sum);
+    obj->cache_obj = (struct LogCache_TypeDef *)MMU_Malloc(sizeof(struct LogCache_TypeDef) * cache_sum);
     if (obj->cache_obj == NULL)
     {
         MMU_Free(obj->cache_obj);
@@ -129,7 +131,7 @@ static bool TaskLog_CreateCache(Log_Monitor_TypeDef *obj, uint8_t cache_sum, uin
 
     for (uint8_t i = 0; i < cache_sum; i++)
     {
-        memset(&(obj->cache_obj)[i], NULL, sizeof(LogCache_TypeDef));
+        memset(&(obj->cache_obj)[i], NULL, sizeof(struct LogCache_TypeDef));
 
         obj->cache_obj[i].p_buf = MMU_Malloc(cache_buff_size);
         if (obj->cache_obj[i].p_buf == NULL)
@@ -140,21 +142,23 @@ static bool TaskLog_CreateCache(Log_Monitor_TypeDef *obj, uint8_t cache_sum, uin
         }
 
         memset(obj->cache_obj[i].p_buf, NULL, cache_buff_size);
+        obj->cache_obj[i].nxt = NULL;
         obj->cache_obj[i].rem_size = cache_buff_size;
         obj->cache_obj[i].tot_size = cache_buff_size;
         obj->cache_obj[i].ocp_size = 0;
-
-        obj->cache_obj[i].page = cache_sum;
-        obj->cache_obj[i].id = i;
 
         for (uint8_t init_index = 0; init_index < init_cnt; init_index++)
         {
             memcpy(&obj->cache_obj[i].p_buf[init_index * obj->single_log_size], &header, sizeof(header));
         }
+
+        if(i > 0)
+        {
+            obj->cache_obj[i - 1].nxt = &obj->cache_obj[i];
+        }
     }
 
     obj->inuse_cache_page = obj->cache_obj;
-    obj->inuse_page_id = 0;
     obj->store_page = NULL;
 
     return true;
@@ -162,16 +166,13 @@ static bool TaskLog_CreateCache(Log_Monitor_TypeDef *obj, uint8_t cache_sum, uin
 
 static void TaskLog_IMU_ToFile(Log_Monitor_TypeDef *log_obj)
 {
-    uint8_t init_cnt = 0;
-
     if (log_obj == NULL || log_obj->store_page == NULL)
         return;
 
-    init_cnt = log_obj->store_page->tot_size / log_obj->single_log_size;
     Disk.write(&FATFS_Obj, &LogFile_Obj, log_obj->store_page->p_buf, log_obj->store_page->ocp_size);
 
     memset(log_obj->store_page->p_buf, NULL, log_obj->store_page->tot_size);
-    for (uint8_t init_index = 0; init_index < init_cnt; init_index++)
+    for (uint8_t init_index = 0; init_index < log_obj->store_page->tot_size / log_obj->single_log_size; init_index++)
     {
         memcpy(&log_obj->store_page->p_buf[init_index * log_obj->single_log_size], &log_obj->log_header, sizeof(log_obj->log_header));
     }
@@ -179,7 +180,12 @@ static void TaskLog_IMU_ToFile(Log_Monitor_TypeDef *log_obj)
     log_obj->store_page->ocp_size = 0;
     log_obj->store_page->rem_size = log_obj->store_page->tot_size;
 
-    log_obj->store_page = NULL;
+    if(log_obj->store_page->nxt == NULL)
+    {
+        log_obj->store_page = NULL;
+    }
+    else
+        log_obj->store_page = log_obj->store_page->nxt;
 }
 
 static void TaskLog_IMUData_Update(Log_Monitor_TypeDef *log_obj, DataPipeObj_TypeDef *pipe_obj)
@@ -189,21 +195,32 @@ static void TaskLog_IMUData_Update(Log_Monitor_TypeDef *log_obj, DataPipeObj_Typ
 
     if (log_obj->inuse_cache_page->rem_size < log_obj->single_log_size)
     {
-        log_obj->store_page = log_obj->inuse_cache_page;
+        if(log_obj->store_page == NULL)
+        {
+            log_obj->store_page = log_obj->inuse_cache_page;
+        }
+        else
+            /* if stil logging then insert to next */
+            log_obj->store_page->nxt = log_obj->inuse_cache_page;
 
-        /* switch cache page */
-        log_obj->inuse_page_id++;
-        log_obj->inuse_page_id %= log_obj->cache_page_sum;
-
-        log_obj->inuse_cache_page = &(log_obj->cache_obj[log_obj->inuse_page_id]);
+        /* switch inuse cache page */
+        if(log_obj->inuse_cache_page->nxt)
+        {
+            log_obj->inuse_cache_page = log_obj->inuse_cache_page->nxt;
+        }
+        else
+            log_obj->inuse_cache_page = log_obj->cache_obj;
     }
+     
+    if(log_obj->inuse_cache_page->rem_size - log_obj->single_log_size >= 0)
+    {
+        memcpy(log_obj->inuse_cache_page->p_buf + log_obj->single_log_offset + log_obj->inuse_cache_page->ocp_size,
+            (uint8_t *)(pipe_obj->data_addr),
+            pipe_obj->data_size);
 
-    memcpy(log_obj->inuse_cache_page->p_buf + log_obj->single_log_offset + log_obj->inuse_cache_page->ocp_size,
-           (uint8_t *)(pipe_obj->data_addr),
-           pipe_obj->data_size);
-
-    log_obj->inuse_cache_page->rem_size -= log_obj->single_log_size;
-    log_obj->inuse_cache_page->ocp_size += log_obj->single_log_size;
+        log_obj->inuse_cache_page->rem_size -= log_obj->single_log_size;
+        log_obj->inuse_cache_page->ocp_size += log_obj->single_log_size;
+    }
 }
 
 static void TaskLog_PipeTransFinish_Callback(DataPipeObj_TypeDef *obj)
--- Task/Task_Log.h
+++ Task/Task_Log.h
@@ -22,30 +22,26 @@ typedef struct
     uint8_t size;
 } LogData_Header_TypeDef;
 
-typedef struct
+struct LogCache_TypeDef
 {
     uint8_t *p_buf;
-    uint16_t rem_size; /* remain size */
-    uint16_t ocp_size; /* occupy size */
+    int16_t rem_size; /* remain size */
+    int16_t ocp_size; /* occupy size */
     uint16_t tot_size; /* total  size */
 
-    uint8_t page;
-    uint8_t id;
-} LogCache_TypeDef;
+    struct LogCache_TypeDef *nxt;
+};
 #pragma pack()
 
 typedef struct
 {
     LogData_Header_TypeDef log_header;
-    LogCache_TypeDef *cache_obj;
-    LogCache_TypeDef *inuse_cache_page;
-    LogCache_TypeDef *store_page;
+    struct LogCache_TypeDef *cache_obj;
+    struct LogCache_TypeDef *inuse_cache_page;
+    struct LogCache_TypeDef *store_page;
 
     uint16_t single_log_size;
     uint16_t single_log_offset;
-
-    uint8_t inuse_page_id;
-    uint8_t cache_page_sum;
 } Log_Monitor_TypeDef;
 
 void TaskLog_Init(void);
