11e581fd3cc54bf24c3ea4f2cf869becb9659169
8BIT0 <971168718@qq.com>
2022-10-24 23:16:51 +0800

    add Analysis_Tool folder and file

---
+++ Analysis_Tool/Log2Txt/code/inc/var_def.h
@@ -0,0 +1,58 @@
+#ifndef __VAR_DEF_H
+#define __VAR_DEF_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+typedef uint64_t SYSTEM_RunTime;
+
+typedef enum
+{
+    Axis_X = 0,
+    Axis_Y,
+    Axis_Z,
+    Axis_Sum,
+} AxisList_TypeDef;
+
+typedef enum
+{
+    SrvIMU_Sample_NoError = 0,
+    SrvIMU_Sample_Module_UnReady,
+    SrvIMU_Sample_Data_Acc_Blunt,
+    SrvIMU_Sample_Data_Gyr_Blunt,
+    SrvIMU_Sample_Data_Acc_OverRange,
+    SrvIMU_Sample_Data_Gyr_OverRange,
+} SrvIMU_SampleErrorCode_List;
+
+#pragma pack(1)
+typedef struct
+{
+    uint8_t header;
+    uint8_t type;
+    uint8_t size;
+} LogData_Header_TypeDef;
+
+typedef struct
+{
+    SYSTEM_RunTime time_stamp;
+    uint64_t cycle_cnt;
+
+    float tempera;
+    float gyr[Axis_Sum];
+    float acc[Axis_Sum];
+
+    SrvIMU_SampleErrorCode_List error_code;
+    uint16_t chk_sum;
+} IMU_Data_TypeDef;
+
+typedef union
+{
+    uint8_t buff[sizeof(IMU_Data_TypeDef)];
+    IMU_Data_TypeDef data;
+} IMU_LogUnionData_TypeDef;
+#pragma pack()
+
+#endif
---
+++ Analysis_Tool/Log2Txt/code/src/main.c
@@ -0,0 +1,40 @@
+#include "../inc/var_def.h"
+
+#define MIN_LOG_FILENAME_LEN 5
+#define EXTEND_FILETYPE_NAME ".log"
+
+bool Load_File(char *path)
+{
+    uint16_t file_path_len = 0;
+
+    if (path)
+    {
+        file_path_len = strlen(path);
+
+        if (file_path_len > MIN_LOG_FILENAME_LEN)
+        {
+            printf("[Error]\tLogFile Path input :\t%s\r\n", path);
+            memset(path, '\0', strlen(path));
+            printf("\r\n\r\n");
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    char *path;
+
+    do
+    {
+        /* waiting for path input */
+        printf("[INFO]\tType LogFile Path : \t");
+        scanf("%s", path);
+    } while (!Load_File(path));
+
+    while (1)
+    {
+    }
+    return 0;
+}
