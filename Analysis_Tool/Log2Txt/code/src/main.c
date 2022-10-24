8acec24c1cbdc7f5ac9edcdb2b8ec79a9d60788b
8BIT0 <971168718@qq.com>
2022-10-24 23:30:11 +0800

    Analysis_Tool update

--- Analysis_Tool/Log2Txt/code/src/main.c
+++ Analysis_Tool/Log2Txt/code/src/main.c
@@ -4,7 +4,27 @@
 #define EXTEND_FILETYPE_NAME ".log"
 #define EXTEND_FILETYPE_NAME_LEN strlen(EXTEND_FILETYPE_NAME)
 
-bool Load_File(char *path)
+static bool File_Check_ExtendType(char *ext_type)
+{
+    char *ext;
+
+    if (ext_type)
+    {
+        ext = ext_type + strlen(ext_type) - EXTEND_FILETYPE_NAME_LEN;
+
+        if (strcmp(ext, EXTEND_FILETYPE_NAME) == 0)
+        {
+            printf("[INFO]\tFile Type Checked\r\n");
+            return true;
+        }
+        else
+            printf("[Error]\tFile Type Error\r\n");
+    }
+
+    return false;
+}
+
+static bool Load_File(char *path)
 {
     uint16_t file_path_len = 0;
 
@@ -12,12 +32,19 @@ bool Load_File(char *path)
     {
         file_path_len = strlen(path);
 
-        if (file_path_len > MIN_LOG_FILENAME_LEN)
+        if ((file_path_len > MIN_LOG_FILENAME_LEN) && File_Check_ExtendType(path))
+        {
+            /* open log file */
+
+            /* read log data into buff */
+        }
+        else
         {
             printf("[Error]\tLogFile Path input :\t%s\r\n", path);
-            memset(path, '\0', strlen(path));
             printf("\r\n\r\n");
         }
+
+        memset(path, '\0', strlen(path));
     }
 
     return false;
@@ -32,6 +59,7 @@ int main()
         /* waiting for path input */
         printf("[INFO]\tType LogFile Path : \t");
         scanf("%s", path);
+        printf("\r\n");
     } while (!Load_File(path));
 
     while (1)
