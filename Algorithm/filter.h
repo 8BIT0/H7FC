8fc0fd485dd46e5524952a443535de30ef46ed5b
‘8BIT0’ <971168718@qq.com>
2022-11-21 17:21:59 +0800

    Add File

---
+++ Algorithm/filter.h
@@ -0,0 +1,28 @@
+#ifndef __FILTER_H
+#define __FILTER_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#pragma pack(1)
+typedef struct
+{
+
+}Filter_LowPassParam_TypeDef;
+
+typedef struct
+{
+    
+}Filter_HighPassParam_TypeDef;
+
+typedef struct
+{
+
+}Filter_KalmenParam_TypeDef;
+#pragma pack()
+
+#endif
+
