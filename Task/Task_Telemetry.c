80ecf129db319988546b8754e34d26a5380524a5
8BIT0 <971168718@qq.com>
2022-12-09 22:50:53 +0800

    add file

---
+++ Task/Task_Telemetry.c
@@ -0,0 +1,9 @@
+#include "Task_Telemetry.h"
+
+void TaskTelemetry_Init(void)
+{
+}
+
+void TaskTelemetry_Core(Task_Handle hdl)
+{
+}
---
+++ Task/Task_Telemetry.h
@@ -0,0 +1,10 @@
+#ifndef __TASK_TELEMETRY_H
+#define __TASK_TELEMETRY_H
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "scheduler.h"
+#include "Srv_Receiver.h"
+
+#endif
