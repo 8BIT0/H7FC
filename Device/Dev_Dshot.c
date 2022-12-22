cf376f6d06119cb5816f566268b852e5c131edd2
‘8BIT0’ <971168718@qq.com>
2022-12-22 17:22:28 +0800

    Dev_Dshot update

--- Device/Dev_Dshot.c
+++ Device/Dev_Dshot.c
@@ -1,11 +1,38 @@
 #include "Dev_Dshot.h"
+#include <math.h>
+
+static uint16_t DevDshot_GetType_Clock(DevDshotType_List type)
+{
+    switch(type)
+    {
+        case DevDshot_150:
+            return DSHOT150_CLK_HZ;
+
+        case DevDshot_300:
+            return DSHOT300_CLK_HZ;
+
+        case DevDshot_600:
+            return DSHOT600_CLK_HZ;
+
+        default:
+            return DSHOT300_CLK_HZ;
+    }
+}
 
 static bool DevDshot_Init(DevDshotObj_TypeDef *obj, DevDshotType_List type, BspGPIO_Obj_TypeDef pin)
 {
-    if(!obj || (type < DevDshot_150) || (type > DevDshot_600))
+	uint16_t prescaler = 0;
+
+    if(!obj)
         return false;
 
-    obj->type = type;
+    if((type < DevDshot_150) || (type > DevDshot_600))
+    {
+        obj->type = DevDshot_300;
+    }
+    else
+        obj->type = type;
+    
     obj->pin = pin;
 
     return true;
