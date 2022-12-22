cf2a86a74e54820e680c1491ba6db315f49bb152
‘8BIT0’ <971168718@qq.com>
2022-12-22 15:02:17 +0800

    Dev_Dshot update

--- Device/Dev_Dshot.h
+++ Device/Dev_Dshot.h
@@ -7,7 +7,19 @@
 
 typedef enum
 {
-    DevDshot_Rotate_Invert = 9,
+    DevDshot_MotoOutput_Lock = 0,
+    DevDshot_Beeper_Level_1 = 1,
+    DevDshot_Beeper_Level_2,
+    DevDshot_Beeper_Level_3,
+    DevDshot_Beeper_Level_4,
+    DevDshot_Beeper_Level_5,
+    DevDshot_Info_Req,
+    DevDshot_RotateDir_ClockWise,
+    DevDshot_RotateDir_AntiClockWise,
+    DevDshot_Disable_3D_Mode,
+    DevDshot_Enable_3D_Mode,
+    DevDshot_Setting_Req,
+    DevDshot_Save_Setting,
 }DevDshot_Command_List; 
 
 typedef enum
