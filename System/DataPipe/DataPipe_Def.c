39516fe165f210d40756802bf2b6aace8d909941
8BIT0 <971168718@qq.com>
2022-12-30 22:04:21 +0800

    DataPipe_Def update

--- System/DataPipe/DataPipe_Def.c
+++ System/DataPipe/DataPipe_Def.c
@@ -1,5 +1,7 @@
 #include "DataPipe.h"
 
 DataPipeObj_TypeDef Receiver_Smp_DataPipe;
+DataPipeObj_TypeDef Receiver_Ctl_DataPipe;
+
 DataPipeObj_TypeDef IMU_Log_DataPipe;
 DataPipeObj_TypeDef IMU_Smp_DataPipe;
