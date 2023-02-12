a0bda132939d07df4cb6f5f945575444688567d7
8_BIT0 <971168718@qq.com>
2023-02-12 22:17:41 +0800

    Task_Control update

--- Task/Task_Control.c
+++ Task/Task_Control.c
@@ -14,7 +14,7 @@
 #define DEFAULT_ESC_TYPE DevDshot_600
 
 DataPipe_CreateDataObj(SrvIMU_UnionData_TypeDef, Filted_IMU_Data);
-DataPipe_CreateDataObj(SrvRecever_RCSig_TypeDef, RCTelemtry_Data);
+DataPipe_CreateDataObj(SrvRecever_RCSig_TypeDef, Control_RC_Data);
 
 TaskControl_Monitor_TypeDef TaskControl_Monitor;
 
@@ -23,6 +23,14 @@ static void TaskControl_DataPipe_Callback(DataPipeObj_TypeDef *obj);
 void TaskControl_Init(void)
 {
     // IMU_Ctl_DataPipe
+    memset(DataPipe_DataObjAddr(Control_RC_Data), 0, DataPipe_DataSize(Control_RC_Data));
+    Receiver_Ctl_DataPipe.data_addr = (uint32_t)DataPipe_DataObjAddr(Control_RC_Data);
+    Receiver_Ctl_DataPipe.data_size = DataPipe_DataSize(Control_RC_Data);
+    Receiver_Ctl_DataPipe.trans_finish_cb = TaskControl_DataPipe_Callback;
+    DataPipe_Enable(&Receiver_Ctl_DataPipe);
+
+    DataPipe_Enable(&IMU_Ctl_DataPipe);
+
     TaskControl_Monitor.init_state = SrvActuator.init(DEFAULT_CONTROL_MODEL, DEFAULT_ESC_TYPE);
 
     if (TaskControl_Monitor.init_state)
