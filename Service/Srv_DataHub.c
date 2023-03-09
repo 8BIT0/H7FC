a83d7c1c72880b5d7fc4b4a4d3ee600f0df068d7
‘8BIT0’ <971168718@qq.com>
2023-03-09 16:49:52 +0800

    Srv_DataHub.c update

--- Service/Srv_DataHub.c
+++ Service/Srv_DataHub.c
@@ -1,5 +1,8 @@
 #include "Srv_DataHub.h"
 
+/* internal variable */
+SrvDataHub_TypeDef DataHub;
+
 /* Pipe Object */
 DataPipe_CreateDataObj(SrvIMU_UnionData_TypeDef, PtlPriIMU_Data);
 DataPipe_CreateDataObj(SrvIMU_UnionData_TypeDef, PtlSecIMU_Data);
@@ -9,14 +12,16 @@ DataPipe_CreateDataObj(SrvRecever_RCSig_TypeDef, Proto_Rc);
 /* internal function */
 static void SrvComProto_PipeRcTelemtryDataFinish_Callback(DataPipeObj_TypeDef *obj);
 
-static bool SrvDataHub_Init()
+static void SrvDataHub_Init(void)
 {
+    memset(&DataHub, 0, sizeof(DataHub));
+
     /* init pipe object */
     memset(DataPipe_DataObjAddr(PtlPriIMU_Data), NULL, DataPipe_DataSize(PtlPriIMU_Data));
     IMU_Ptl_DataPipe.data_addr = (uint32_t)DataPipe_DataObjAddr(PtlPriIMU_Data);
     IMU_Ptl_DataPipe.data_size = DataPipe_DataSize(PtlPriIMU_Data);
     IMU_Ptl_DataPipe.trans_finish_cb = SrvComProto_PipeRcTelemtryDataFinish_Callback;
-    DataPipe_Set_RxInterval(&IMU_Ptl_DataPipe, Runtime_MsToUs(5)); /* limit pipe frequence to 200Hz */
+    // DataPipe_Set_RxInterval(&IMU_Ptl_DataPipe, Runtime_MsToUs(5)); /* limit pipe frequence to 200Hz */
     DataPipe_Enable(&IMU_Ptl_DataPipe);
 
     /* init pipe object */
@@ -24,7 +29,7 @@ static bool SrvDataHub_Init()
     Receiver_ptl_DataPipe.data_addr = (uint32_t)DataPipe_DataObjAddr(Proto_Rc);
     Receiver_ptl_DataPipe.data_size = DataPipe_DataSize(Proto_Rc);
     Receiver_ptl_DataPipe.trans_finish_cb = SrvComProto_PipeRcTelemtryDataFinish_Callback;
-    DataPipe_Set_RxInterval(&Receiver_ptl_DataPipe, Runtime_MsToUs(20)); /* limit pipe frequence to 50Hz */
+    // DataPipe_Set_RxInterval(&Receiver_ptl_DataPipe, Runtime_MsToUs(20)); /* limit pipe frequence to 50Hz */
     DataPipe_Enable(&Receiver_ptl_DataPipe);
 }
 
