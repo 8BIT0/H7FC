b5e1a06589520a27f1a216eb05ddff7c6c1f0788
8_BIT0 <971168718@qq.com>
2023-03-23 23:04:45 +0800

    Task_Telemetry update

--- Task/Task_Telemetry.h
+++ Task/Task_Telemetry.h
@@ -72,6 +72,10 @@ typedef struct
 
 typedef struct
 {
+    bool enable_deadzone;
+    uint16_t center_deadzone_min;
+    uint16_t center_deadzone_max;
+
     uint16_t min;
     uint16_t max;
     uint16_t *channel_ptr;
