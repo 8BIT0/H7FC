5cb76f1f8b40ff5ac15fca941658b35b8b94feed
‘8BIT0’ <971168718@qq.com>
2023-04-13 21:35:25 +0800

    filter.c/h update

--- Algorithm/filter.c
+++ Algorithm/filter.c
@@ -4,3 +4,44 @@
 
 #include "filter.h"
 #include "system_cfg.h"
+#include "mmu.h"
+
+static BWF_Object_TypeDef Buterworth_Filter_Init(uint32_t sample_freq, uint8_t order, float *e_para, float *u_para)
+{
+    BWF_Object_TypeDef obj = 0;
+    Filter_ButterworthParam_TypeDef *BWF_Obj = NULL;
+
+    if(sample_freq && order)
+    {
+        if((e_para == NULL) || (u_para == NULL))
+            return 0;
+
+        BWF_Obj = MMU_Malloc(sizeof(Filter_ButterworthParam_TypeDef));
+        
+        if(BWF_Obj == NULL)
+            return 0;
+
+        BWF_Obj->p_e_data_cache = MMU_Malloc(sizeof(float) * order);
+        if(BWF_Obj->p_e_data_cache == NULL)
+        {
+            MMU_Free(BWF_Obj->p_e_data_cache);
+            return 0;
+        }
+        memset(BWF_Obj->p_e_data_cache, 0, sizeof(float) * order);
+
+        BWF_Obj->p_u_data_cache = MMU_Malloc(sizeof(float) * order);
+        if(BWF_Obj->p_u_data_cache == NULL)
+        {
+            MMU_Free(BWF_Obj->p_u_data_cache);
+            return 0;
+        }
+        memset(BWF_Obj->p_u_data_cache, 0, sizeof(float) * order);
+
+        BWF_Obj->e_para_buf = e_para;
+        BWF_Obj->u_para_buf = u_para;
+
+        return obj;
+    }
+
+    return 0;
+}
--- Algorithm/filter.h
+++ Algorithm/filter.h
@@ -10,6 +10,8 @@
 
 #define SMOOTH_WINDOW_SIZE 5
 
+typedef uint32_t BWF_Object_TypeDef; /* butterworth filter object */
+
 #pragma pack(1)
 typedef struct
 {
@@ -17,8 +19,12 @@ typedef struct
     uint32_t stop_freq;
 
     uint8_t order;
-    float *p_data_cache;
-    float *p_para_cache;
+
+    float *e_para_buf;
+    float *u_para_buf;
+
+    float *p_e_data_cache;
+    float *p_u_data_cache;
 }Filter_ButterworthParam_TypeDef;
 
 typedef struct
