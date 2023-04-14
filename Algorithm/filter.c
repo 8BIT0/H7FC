6f19adb5b8c651af3cebec18752830eb470e9a7f
‘8BIT0’ <971168718@qq.com>
2023-04-14 14:05:36 +0800

    filter.c update

--- Algorithm/filter.c
+++ Algorithm/filter.c
@@ -117,13 +117,14 @@ static BWF_Object_TypeDef Butterworth_Init(uint32_t sample_freq, uint8_t stop_fr
 
 static float Butterworth_Filter_Update(BWF_Object_TypeDef obj, float cur_e)
 {
+    Filter_ButterworthParam_TypeDef *filter_obj = NULL;
     float u_tmp = 0.0f;
     float E_Additive = 0.0f;
     float U_Additive = 0.0f;
 
     if(obj)
     {
-        
+        filter_obj = (Filter_ButterworthParam_TypeDef *)obj;
     }
 
     return u_tmp;
