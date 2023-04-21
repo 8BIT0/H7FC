9172c4e0230f2feadcef5802e5afad2ef85c61b8
‘8BIT0’ <971168718@qq.com>
2023-04-21 14:14:53 +0800

    filter_param update

--- Algorithm/filter_param.c
+++ Algorithm/filter_param.c
@@ -12,103 +12,103 @@
  * 1K Sample Rate Section
  */
 /* 2 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 1K */
-const float BTF_E_2O_30Hz_1K[3];
-const float BTF_U_2O_30Hz_1K[2];
+const BTF_Para_TypeDef BTF_E_2O_30Hz_1K[3];
+const BTF_Para_TypeDef BTF_U_2O_30Hz_1K[2];
 
 /* 2 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 1K */
-const float BTF_E_2O_50Hz_1K[3];
-const float BTF_U_2O_50Hz_1K[2];
+const BTF_Para_TypeDef BTF_E_2O_50Hz_1K[3];
+const BTF_Para_TypeDef BTF_U_2O_50Hz_1K[2];
 
 /* 3 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 1K */
-const float BTF_E_3O_30Hz_1K[4];
-const float BTF_U_3O_30Hz_1K[3];
+const BTF_Para_TypeDef BTF_E_3O_30Hz_1K[4];
+const BTF_Para_TypeDef BTF_U_3O_30Hz_1K[3];
 
 /* 3 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 1K */
-const float BTF_E_3O_50Hz_1K[4];
-const float BTF_U_3O_50Hz_1K[3];
+const BTF_Para_TypeDef BTF_E_3O_50Hz_1K[4];
+const BTF_Para_TypeDef BTF_U_3O_50Hz_1K[3];
 
 /* 4 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 1K */
-const float BTF_E_4O_30Hz_1K[5];
-const float BTF_U_4O_30Hz_1K[4];
+const BTF_Para_TypeDef BTF_E_4O_30Hz_1K[5];
+const BTF_Para_TypeDef BTF_U_4O_30Hz_1K[4];
 
 /* 4 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 1K */
-const float BTF_E_4O_50Hz_1K[5];
-const float BTF_U_4O_50Hz_1K[4];
+const BTF_Para_TypeDef BTF_E_4O_50Hz_1K[5];
+const BTF_Para_TypeDef BTF_U_4O_50Hz_1K[4];
 
 /* 5 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 1K */
-const float BTF_E_5O_30Hz_1K[6];
-const float BTF_U_5O_30Hz_1K[5];
+const BTF_Para_TypeDef BTF_E_5O_30Hz_1K[6];
+const BTF_Para_TypeDef BTF_U_5O_30Hz_1K[5];
 
 /* 5 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 1K */
-const float BTF_E_5O_50Hz_1K[6];
-const float BTF_U_5O_50Hz_1K[5];
+const BTF_Para_TypeDef BTF_E_5O_50Hz_1K[6];
+const BTF_Para_TypeDef BTF_U_5O_50Hz_1K[5];
 
 /*
  * 2K Sample Rate section
  */
 /* 2 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 2K */
-const float BTF_E_2O_30Hz_2K[3];
-const float BTF_U_2O_30Hz_2K[2];
+const BTF_Para_TypeDef BTF_E_2O_30Hz_2K[3];
+const BTF_Para_TypeDef BTF_U_2O_30Hz_2K[2];
 
 /* 2 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 2K */
-const float BTF_E_2O_50Hz_2K[3];
-const float BTF_U_2O_50Hz_2K[2];
+const BTF_Para_TypeDef BTF_E_2O_50Hz_2K[3];
+const BTF_Para_TypeDef BTF_U_2O_50Hz_2K[2];
 
 /* 3 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 2K */
-const float BTF_E_3O_30Hz_2K[4];
-const float BTF_U_3O_30Hz_2K[3];
+const BTF_Para_TypeDef BTF_E_3O_30Hz_2K[4];
+const BTF_Para_TypeDef BTF_U_3O_30Hz_2K[3];
 
 /* 3 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 2K */
-const float BTF_E_3O_50Hz_2K[4];
-const float BTF_U_3O_50Hz_2K[3];
+const BTF_Para_TypeDef BTF_E_3O_50Hz_2K[4];
+const BTF_Para_TypeDef BTF_U_3O_50Hz_2K[3];
 
 /* 4 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 2K */
-const float BTF_E_4O_30Hz_2K[5];
-const float BTF_U_4O_30Hz_2K[4];
+const BTF_Para_TypeDef BTF_E_4O_30Hz_2K[5];
+const BTF_Para_TypeDef BTF_U_4O_30Hz_2K[4];
 
 /* 4 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 2K */
-const float BTF_E_4O_50Hz_2K[5];
-const float BTF_U_4O_50Hz_2K[4];
+const BTF_Para_TypeDef BTF_E_4O_50Hz_2K[5];
+const BTF_Para_TypeDef BTF_U_4O_50Hz_2K[4];
 
 /* 5 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 2K */
-const float BTF_E_5O_30Hz_2K[6];
-const float BTF_U_5O_30Hz_2K[5];
+const BTF_Para_TypeDef BTF_E_5O_30Hz_2K[6];
+const BTF_Para_TypeDef BTF_U_5O_30Hz_2K[5];
 
 /* 5 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 2K */
-const float BTF_E_5O_50Hz_2K[6] = {2.34099149306140e-06, 1.17049574653070e-05, 2.34099149306140e-05, 2.34099149306140e-05, 1.17049574653070e-05, 2.34099149306140e-06};
-const float BTF_U_5O_50Hz_2K[5] = {-4.49183096507705, 8.09405541782665, -7.31208128015038, 3.31104756198840, -0.601115822859839};
+const BTF_Para_TypeDef BTF_E_5O_50Hz_2K[6] = {{2.3409914930614, 1e-06}, {1.1704957465307, 1e-05}, {2.3409914930614, 1e-05}, {2.3409914930614, 1e-05}, {1.1704957465307, 1e-05}, {2.3409914930614, 1e-06}};
+const BTF_Para_TypeDef BTF_U_5O_50Hz_2K[5] = {{-4.49183096507705, DEFAULT_PARAM_SCALE}, {8.09405541782665, DEFAULT_PARAM_SCALE}, {-7.31208128015038, DEFAULT_PARAM_SCALE}, {3.31104756198840, DEFAULT_PARAM_SCALE}, {-0.601115822859839, DEFAULT_PARAM_SCALE}};
 
 /*
  * 4K Sample Rate section
  */
 /* 2 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 4K */
-const float BTF_E_2O_30Hz_4K[3];
-const float BTF_U_2O_30Hz_4K[2];
+const BTF_Para_TypeDef BTF_E_2O_30Hz_4K[3];
+const BTF_Para_TypeDef BTF_U_2O_30Hz_4K[2];
 
 /* 2 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 4K */
-const float BTF_E_2O_50Hz_4K[3];
-const float BTF_U_2O_50Hz_4K[2];
+const BTF_Para_TypeDef BTF_E_2O_50Hz_4K[3];
+const BTF_Para_TypeDef BTF_U_2O_50Hz_4K[2];
 
 /* 3 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 4K */
-const float BTF_E_3O_30Hz_4K[4];
-const float BTF_U_3O_30Hz_4K[3];
+const BTF_Para_TypeDef BTF_E_3O_30Hz_4K[4];
+const BTF_Para_TypeDef BTF_U_3O_30Hz_4K[3];
 
 /* 3 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 4K */
-const float BTF_E_3O_50Hz_4K[4];
-const float BTF_U_3O_50Hz_4K[3];
+const BTF_Para_TypeDef BTF_E_3O_50Hz_4K[4];
+const BTF_Para_TypeDef BTF_U_3O_50Hz_4K[3];
 
 /* 4 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 4K */
-const float BTF_E_4O_30Hz_4K[5];
-const float BTF_U_4O_30Hz_4K[4];
+const BTF_Para_TypeDef BTF_E_4O_30Hz_4K[5];
+const BTF_Para_TypeDef BTF_U_4O_30Hz_4K[4];
 
 /* 4 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 4K */
-const float BTF_E_4O_50Hz_4K[5];
-const float BTF_U_4O_50Hz_4K[4];
+const BTF_Para_TypeDef BTF_E_4O_50Hz_4K[5];
+const BTF_Para_TypeDef BTF_U_4O_50Hz_4K[4];
 
 /* 5 order butterworth filter parameter / stop frequence: 30Hz / sample rate: 4K */
-const float BTF_E_5O_30Hz_4K[6];
-const float BTF_U_5O_30Hz_4K[5];
+const BTF_Para_TypeDef BTF_E_5O_30Hz_4K[6];
+const BTF_Para_TypeDef BTF_U_5O_30Hz_4K[5];
 
 /* 5 order butterworth filter parameter / stop frequence: 50Hz / sample rate: 4K */
-const float BTF_E_5O_50Hz_4K[6];
-const float BTF_U_5O_50Hz_4K[5];
+const BTF_Para_TypeDef BTF_E_5O_50Hz_4K[6];
+const BTF_Para_TypeDef BTF_U_5O_50Hz_4K[5];
--- Algorithm/filter_param.h
+++ Algorithm/filter_param.h
@@ -4,76 +4,84 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-extern const float BTF_E_2O_30Hz_1K[3];
-extern const float BTF_U_2O_30Hz_1K[2];
+#define DEFAULT_PARAM_SCALE 1.0f
 
-extern const float BTF_E_2O_50Hz_1K[3];
-extern const float BTF_U_2O_50Hz_1K[2];
+typedef struct
+{
+    float p;
+    float scale;
+}BTF_Para_TypeDef;
 
-extern const float BTF_E_3O_30Hz_1K[4];
-extern const float BTF_U_3O_30Hz_1K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_30Hz_1K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_30Hz_1K[2];
 
-extern const float BTF_E_3O_50Hz_1K[4];
-extern const float BTF_U_3O_50Hz_1K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_50Hz_1K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_50Hz_1K[2];
 
-extern const float BTF_E_4O_30Hz_1K[5];
-extern const float BTF_U_4O_30Hz_1K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_30Hz_1K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_30Hz_1K[3];
 
-extern const float BTF_E_4O_50Hz_1K[5];
-extern const float BTF_U_4O_50Hz_1K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_50Hz_1K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_50Hz_1K[3];
 
-extern const float BTF_E_5O_30Hz_1K[6];
-extern const float BTF_U_5O_30Hz_1K[5];
+extern const BTF_Para_TypeDef BTF_E_4O_30Hz_1K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_30Hz_1K[4];
 
-extern const float BTF_E_5O_50Hz_1K[6];
-extern const float BTF_U_5O_50Hz_1K[5];
+extern const BTF_Para_TypeDef BTF_E_4O_50Hz_1K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_50Hz_1K[4];
 
-extern const float BTF_E_2O_30Hz_2K[3];
-extern const float BTF_U_2O_30Hz_2K[2];
+extern const BTF_Para_TypeDef BTF_E_5O_30Hz_1K[6];
+extern const BTF_Para_TypeDef BTF_U_5O_30Hz_1K[5];
 
-extern const float BTF_E_2O_50Hz_2K[3];
-extern const float BTF_U_2O_50Hz_2K[2];
+extern const BTF_Para_TypeDef BTF_E_5O_50Hz_1K[6];
+extern const BTF_Para_TypeDef BTF_U_5O_50Hz_1K[5];
 
-extern const float BTF_E_3O_30Hz_2K[4];
-extern const float BTF_U_3O_30Hz_2K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_30Hz_2K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_30Hz_2K[2];
 
-extern const float BTF_E_3O_50Hz_2K[4];
-extern const float BTF_U_3O_50Hz_2K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_50Hz_2K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_50Hz_2K[2];
 
-extern const float BTF_E_4O_30Hz_2K[5];
-extern const float BTF_U_4O_30Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_30Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_30Hz_2K[3];
 
-extern const float BTF_E_4O_50Hz_2K[5];
-extern const float BTF_U_4O_50Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_50Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_50Hz_2K[3];
 
-extern const float BTF_E_5O_30Hz_2K[5];
-extern const float BTF_U_5O_30Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_E_4O_30Hz_2K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_30Hz_2K[4];
 
-extern const float BTF_E_5O_50Hz_2K[5];
-extern const float BTF_U_5O_50Hz_2K[4];
+extern const BTF_Para_TypeDef BTF_E_4O_50Hz_2K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_50Hz_2K[4];
 
-extern const float BTF_E_2O_30Hz_4K[3];
-extern const float BTF_U_2O_30Hz_4K[2];
+extern const BTF_Para_TypeDef BTF_E_5O_30Hz_2K[5];
+extern const BTF_Para_TypeDef BTF_U_5O_30Hz_2K[4];
 
-extern const float BTF_E_2O_50Hz_4K[3];
-extern const float BTF_U_2O_50Hz_4K[2];
+extern const BTF_Para_TypeDef BTF_E_5O_50Hz_2K[5];
+extern const BTF_Para_TypeDef BTF_U_5O_50Hz_2K[4];
 
-extern const float BTF_E_3O_30Hz_4K[4];
-extern const float BTF_U_3O_30Hz_4K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_30Hz_4K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_30Hz_4K[2];
 
-extern const float BTF_E_3O_50Hz_4K[4];
-extern const float BTF_U_3O_50Hz_4K[3];
+extern const BTF_Para_TypeDef BTF_E_2O_50Hz_4K[3];
+extern const BTF_Para_TypeDef BTF_U_2O_50Hz_4K[2];
 
-extern const float BTF_E_4O_30Hz_4K[5];
-extern const float BTF_U_4O_30Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_30Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_30Hz_4K[3];
 
-extern const float BTF_E_4O_50Hz_4K[5];
-extern const float BTF_U_4O_50Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_E_3O_50Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_U_3O_50Hz_4K[3];
 
-extern const float BTF_E_5O_30Hz_4K[5];
-extern const float BTF_U_5O_30Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_E_4O_30Hz_4K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_30Hz_4K[4];
 
-extern const float BTF_E_5O_50Hz_4K[5];
-extern const float BTF_U_5O_50Hz_4K[4];
+extern const BTF_Para_TypeDef BTF_E_4O_50Hz_4K[5];
+extern const BTF_Para_TypeDef BTF_U_4O_50Hz_4K[4];
+
+extern const BTF_Para_TypeDef BTF_E_5O_30Hz_4K[5];
+extern const BTF_Para_TypeDef BTF_U_5O_30Hz_4K[4];
+
+extern const BTF_Para_TypeDef BTF_E_5O_50Hz_4K[5];
+extern const BTF_Para_TypeDef BTF_U_5O_50Hz_4K[4];
 
 #endif
