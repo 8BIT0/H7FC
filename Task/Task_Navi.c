234fa3000c29cfc954e348abef9f892ae4d1ceac
‘8BIT0’ <971168718@qq.com>
2023-07-20 11:20:21 +0800

    update

--- Task/Task_Navi.c
+++ Task/Task_Navi.c
@@ -1,5 +1,16 @@
 #include "Task_Navi.h"
+#include "DataPipe.h"
+#include "Srv_Baro.h"
+#include "Srv_IMUSample.h"
 
+/* internal vriable */
+uint32_t TaskNavi_Period = 0;
+
+void TaskNavi_Init(uint32_t period)
+{
+
+    TaskNavi_Period = period;
+}
 
 
 
