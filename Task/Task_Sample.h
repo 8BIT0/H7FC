e67b7bc992a177d8c621ddfc795c1c6bd8d3c117
‘8BIT0’ <971168718@qq.com>
2023-10-26 16:45:44 +0800

    Task_Sample update

--- Task/Task_Sample.h
+++ Task/Task_Sample.h
@@ -18,6 +18,35 @@ typedef enum
 
 typedef enum
 {
+    Task_SensorField_IMU   = UTIL_SET_BIT(0),
+    Task_SensorField_MAG   = UTIL_SET_BIT(1),
+    Task_SensorField_BARO  = UTIL_SET_BIT(2),
+    Task_SensorField_SONAR = UTIL_SET_BIT(3),
+    Task_SensorField_TOF   = UTIL_SET_BIT(4),
+    Task_SensorField_GNSS  = UTIL_SET_BIT(5),
+}Task_SensorMonitor_Field_List;
+
+typedef struct
+{
+    uint32_t min_sample_tick;   /* unit: us   minimun timer tick value when manipulate the bus when sensor sampling */
+    uint32_t max_sample_tick;   /* unit: us   maxmun  timer tick value when manipulate the bus when sensor sampling */
+
+    uint32_t sample_cnt;
+    uint32_t err_cnt;
+    uint32_t period;
+}Task_SensoSampleStatistic_TypeDef;
+
+typedef struct
+{
+    bool enable_monitor_timer;
+    uint32_t enabled_reg;
+    uint8_t enable_num;
+    uint32_t init_state_reg;
+    
+}Task_SensorMonitor_TypeDef;
+
+typedef enum
+{
     Task_SensorBaro_Core = 0,
     Task_SensorBaro_Error,
 }Task_SensorBaro_State;
