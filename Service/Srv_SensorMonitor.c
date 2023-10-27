23a7b169a25836ed813ebc24906c65829fcd8cfe
‘8BIT0’ <971168718@qq.com>
2023-10-27 19:16:42 +0800

    Srv_SensorMonitor update

--- Service/Srv_SensorMonitor.c
+++ Service/Srv_SensorMonitor.c
@@ -1,5 +1,6 @@
 #include "Srv_SensorMonitor.h"
 #include "Bsp_Timer.h"
+#include "Srv_OsCommon.h"
 
 /* 
  * for sensor statistic function a timer is essenial
@@ -10,40 +11,52 @@ static uint32_t SrvSensorMonitor_Get_FreqVal(uint8_t freq_enum);
 
 static bool SrvSensorMonitor_Init(SrvSensorMonitorObj_TypeDef *obj)
 {
+    uint32_t freq_tmp = 0;
+    uint8_t enable_sensor_num = 0;
+
     if(obj)
     {
+        enable_sensor_num = Get_OnSet_Bit_Num(obj->enabled_reg);
+        
+        obj->statistic_list = SrvOsCommon.malloc(enable_sensor_num * sizeof(SrvSensorMonitor_Statistic_TypeDef));
+        if(obj->statistic_list == NULL)
+        {
+            SrvOsCommon.free(obj->statistic_list);
+            return false;
+        }
+
         /* enabled on imu must be essential */
         if(obj->enabled_reg.bit.imu)
         {
-
+            freq_tmp = SrvSensorMonitor_Get_FreqVal(obj->freq_reg.bit.imu);
         }
         else
             return false;
 
         if(obj->enabled_reg.bit.mag)
         {
-
+            freq_tmp = SrvSensorMonitor_Get_FreqVal(obj->freq_reg.bit.mag);
         }
         else
             obj->init_state_reg.bit.mag = false;
 
         if(obj->enabled_reg.bit.baro)
         {
-
+            freq_tmp = SrvSensorMonitor_Get_FreqVal(obj->freq_reg.bit.baro);
         }
         else
             obj->init_state_reg.bit.baro = false;
 
         if(obj->enabled_reg.bit.tof)
         {
-
+            freq_tmp = SrvSensorMonitor_Get_FreqVal(obj->freq_reg.bit.tof);
         }
         else
             obj->init_state_reg.bit.tof = false;
 
         if(obj->enbaled_reg.bit.gnss)
         {
-
+            freq_tmp = SrvSensorMonitor_Get_FreqVal(obj->freq_reg.bit.gnss);
         }
         else
             obj->init_state_reg.bit.gnss = false;
