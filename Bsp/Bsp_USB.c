f330f810a38b7482289000136fec6a99eaad8b64
‘8BIT0’ <971168718@qq.com>
2023-11-23 18:07:56 +0800

    Bsp_USB update

--- Bsp/Bsp_USB.c
+++ Bsp/Bsp_USB.c
@@ -40,6 +40,7 @@ static BspUSB_Error_List BspUSB_VCP_Init(void)
         usb_setrec_callback(BspUSB_VCP_RecData_Callback);
         usb_settxcpl_callback(BspUSB_VCP_SendData_CPLT_Callback);
         
+        memset(BspUSB_VCPMonitor.single_tx_buffer, 0, USB_VCP_MAX_TX_SIZE);
         BspUSB_VCPMonitor.init_state = BspUSB_Error_None;
         return BspUSB_Error_None;
     }
@@ -49,23 +50,38 @@ static BspUSB_Error_List BspUSB_VCP_Init(void)
 
 static BspUSB_Error_List BspUSB_VCP_SendData(uint8_t *p_data, uint16_t len)
 {
-    bool to_queue = false;
+    uint16_t push_size = 0;
+    uint16_t tx_size = 0;
+    uint8_t *push_src_addr = NULL;
 
-    if((BspUSB_VCPMonitor.init_state ==  BspUSB_Error_None) && p_data && len)
+    if((BspUSB_VCPMonitor.init_state == BspUSB_Error_None) && p_data && len)
     {
         if(BspUSB_VCPMonitor.tx_cnt == BspUSB_VCPMonitor.tx_fin_cnt)
         {
-            if()
+            if(len > USB_VCP_MAX_TX_SIZE)
             {
-                to_queue = true;
+                push_size = len - USB_VCP_MAX_TX_SIZE;
+                push_src_addr = &p_data[USB_VCP_MAX_TX_SIZE];
+
+                tx_size = USB_VCP_MAX_TX_SIZE;
+            }
+            else
+                tx_size = len;
+
+            if(CDC_Transmit_FS(p_data, tx_size) != USBD_OK)
+            {
+                
             }
             else
                 BspUSB_VCPMonitor.tx_cnt ++;
         }
         else
-            to_queue = true;
-        
-        if(to_queue)
+        {
+            push_size = len;
+            push_src_addr = p_data;
+        }
+
+        if(push_size && push_src_addr)
         {
             /* push current send into queue for next time sending */
         }
