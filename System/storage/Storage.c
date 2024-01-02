830e748591fd3e7f2f9733c37623cfdaf0e59dc5
root <971168718@qq.com>
2024-01-02 16:16:54 +0800

    update

--- System/storage/Storage.c
+++ System/storage/Storage.c
@@ -8,18 +8,26 @@ static bool Storage_Init(Storage_ModuleState_TypeDef enable)
     memset(&Storage_Monitor, 0, sizeof(Storage_Monitor));
 
     Storage_Monitor.module_enable_reg.val = enable.val;
+    Storage_Monitor.module_init_reg.val = 0;
 
-    if(enable.bit.internal)
+    if(enable.bit.internal && BspFlash.init && BspFlash.init())
     {
         /* on chip flash init */
     }
 
+    /* still in developping */
     if(enable.bit.external)
-    {    
+    {
         /* external flash init */
     }
 
-    return ;
+    Storage_Monitor.init_state = Storage_Monitor.module_init_reg.bit.external | Storage_Monitor.module_init_reg.bit.internal;
+
+    return Storage_Monitor.init_state;
 }
 
+static bool Storage_Build_StorageInfo()
+{
+    
+}
 
--- System/storage/Storage.h
+++ System/storage/Storage.h
@@ -12,6 +12,24 @@
 
 #define ExternalFlash_Storage_Address
 
+#define INTERNAL_STORAGE_PAGE_TAG "[Internal Storage]"
+#define EXTERNAL_STORAGE_PAGE_TAG "[External Storage]"
+
+typedef enum
+{
+    Internal_Flash = 0,
+    External_Flash,
+} Storage_MediumType_List;
+
+typedef struct
+{
+    uint32_t addr;
+    uint32_t start_map_addr;
+    uint8_t map_page_num;
+    uint32_t info_page_size;
+    uint32_t free_block_addr;
+} Storage_InfoPage_TypeDef;
+
 typedef union
 {
     struct
