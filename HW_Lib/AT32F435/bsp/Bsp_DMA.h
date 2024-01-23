ff7b18cd84cf2803aab1596ea7ffcd8b13a2176e
root <971168718@qq.com>
2024-01-23 15:55:53 +0800

    Bsp_DMA update

--- HW_Lib/AT32F435/bsp/Bsp_DMA.h
+++ HW_Lib/AT32F435/bsp/Bsp_DMA.h
@@ -3,6 +3,28 @@
 
 #include "Bsp_DMA_Port_Def.h"
 
+typedef enum
+{
+    Bsp_DMA_None = -1,
+    Bsp_DMA_1 = 0,
+    Bsp_DMA_2,
+    Bsp_DMA_Sum,
+} BspDMA_List;
+
+typedef enum
+{
+    Bsp_DMA_Stream_None = -1,
+    Bsp_DMA_Stream_0 = 0,
+    Bsp_DMA_Stream_1,
+    Bsp_DMA_Stream_2,
+    Bsp_DMA_Stream_3,
+    Bsp_DMA_Stream_4,
+    Bsp_DMA_Stream_5,
+    Bsp_DMA_Stream_6,
+    Bsp_DMA_Stream_7,
+    Bsp_DMA_Stream_Sum,
+} BspDMA_Stream_List;
+
 void BspDMA_Pipe_Irq_Callback(void);
 
 extern BspDMA_Pipe_TypeDef BspDMA_Pipe;
