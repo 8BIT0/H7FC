ceb97fda3abd6d7ed17d5644657895f29c28dd6f
root <971168718@qq.com>
2024-01-29 17:24:48 +0800

    FreeRTOSConfig update

--- System/FreeRTOS/FreeRTOSConfig.h
+++ System/FreeRTOS/FreeRTOSConfig.h
@@ -50,7 +50,11 @@
 /* Ensure definitions are only used by the compiler, and not by the assembler. */
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
   #include <stdint.h>
+  #if !defined AT32F435RGT7
   extern uint32_t SystemCoreClock;
+  #elif defined AT32F435RGT7
+  static uint32_t systemcoreclock = 288000000;
+  #endif
   void xPortSysTickHandler(void);
 #endif
 #define configENABLE_FPU                         1
@@ -61,7 +65,11 @@
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
+#if !defined AT32F435RGT7
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
+#elif defined AT32F435RGT7
+#define configCPU_CLOCK_HZ                       ( systemcoreclock )
+#endif
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 5 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
