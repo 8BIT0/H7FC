022719d451ead5abacdd713ebe8866d1c2122e22
8_BIT0 <971168718@qq.com>
2024-02-01 22:15:44 +0800

    Kernel_at32f435 update

--- HW_Lib/AT32F435/device_support/at32f435_437_conf.h
+++ HW_Lib/AT32F435/device_support/at32f435_437_conf.h
@@ -53,32 +53,32 @@ extern "C"
 /* module define -------------------------------------------------------------*/
 #define CRM_MODULE_ENABLED
 #define TMR_MODULE_ENABLED
-#define ERTC_MODULE_ENABLED
+// #define ERTC_MODULE_ENABLED
 #define GPIO_MODULE_ENABLED
 #define I2C_MODULE_ENABLED
 #define USART_MODULE_ENABLED
 #define PWC_MODULE_ENABLED
 #define CAN_MODULE_ENABLED
 #define ADC_MODULE_ENABLED
-#define DAC_MODULE_ENABLED
+// #define DAC_MODULE_ENABLED
 #define SPI_MODULE_ENABLED
-#define EDMA_MODULE_ENABLED
+// #define EDMA_MODULE_ENABLED
 #define DMA_MODULE_ENABLED
 #define DEBUG_MODULE_ENABLED
 #define FLASH_MODULE_ENABLED
 #define CRC_MODULE_ENABLED
-#define WWDT_MODULE_ENABLED
+// #define WWDT_MODULE_ENABLED
 #define WDT_MODULE_ENABLED
 #define EXINT_MODULE_ENABLED
 #define SDIO_MODULE_ENABLED
-#define XMC_MODULE_ENABLED
+// #define XMC_MODULE_ENABLED
 #define USB_MODULE_ENABLED
 #define ACC_MODULE_ENABLED
 #define MISC_MODULE_ENABLED
-#define QSPI_MODULE_ENABLED
+// #define QSPI_MODULE_ENABLED
 #define DVP_MODULE_ENABLED
 #define SCFG_MODULE_ENABLED
-#define EMAC_MODULE_ENABLED
+// #define EMAC_MODULE_ENABLED
 
 /* includes ------------------------------------------------------------------*/
 #ifdef CRM_MODULE_ENABLED
--- System/kernel/kernel_at32f435.c
+++ System/kernel/kernel_at32f435.c
@@ -1,6 +1,11 @@
 #include "at32f435_437.h"
 #include <stdbool.h>
 
+#define Kernel_DisableIRQ() __asm("cpsid i")
+#define Kernel_EnableIRQ() __asm("cpsie i")
+
+static bool Kernel_TickTimer_Init = false;
+
 bool Kernel_Init(void)
 {
     system_clock_config();
@@ -27,11 +32,59 @@ bool Kernel_Init(void)
     /* enable tmr1 */
     tmr_counter_enable(TMR20, TRUE);
 
+    Kernel_TickTimer_Init = true;
+
     return true;
 }
 
 void Kernel_reboot(void)
 {
+    __set_FAULTMASK(1);
+
+    NVIC_SystemReset();
+}
+
+bool Kernel_EnableTimer_IRQ(void)
+{
 
 }
 
+bool Kernel_DisableTimer_IRQ(void)
+{
+
+}
+
+bool Kernel_Set_PeriodValue(uint32_t value)
+{
+
+}
+
+bool Kernel_Set_SysTimer_TickUnit(uint32_t unit)
+{
+    if(Kernel_TickTimer_Init)
+    {
+
+    }
+
+    return false;
+}
+
+uint32_t Kernel_Get_SysTimer_TickUnit(void)
+{
+    if(Kernel_TickTimer_Init)
+    {
+
+    }
+
+    return 0;
+}
+
+uint32_t Kernel_Get_PeriodValue(void)
+{
+
+}
+
+uint32_t Kernel_TickVal_To_Us(void)
+{
+
+}
