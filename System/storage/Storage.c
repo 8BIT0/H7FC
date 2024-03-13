38c42681902dd365b1722976e0d5a0538872c3bf
root <971168718@qq.com>
2024-03-13 14:42:47 +0800

    Storage update

--- System/storage/Storage.c
+++ System/storage/Storage.c
@@ -980,9 +980,8 @@ static bool Storage_DeleteSingalDataSlot(uint32_t slot_addr, uint8_t *p_data, St
          * link free slot address
          *
          * should link free slot
-         * not set next free slot address only
-         * still bug */
-        *(uint32_t *)p_freeslot_start = p_Sec->free_slot_addr;
+         */
+        *(uint32_t *)p_freeslot_start = 0;
         p_freeslot_start += sizeof(uint32_t);
 
         /* set end tag */
