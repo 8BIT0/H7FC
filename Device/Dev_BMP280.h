e8a6a855e0d540acfe0f8d73a66b2abb413f8e95
root <971168718@qq.com>
2024-03-29 09:40:53 +0800

    Dev_BMP280 update

--- Device/Dev_BMP280.h
+++ Device/Dev_BMP280.h
@@ -5,39 +5,39 @@
 #include <stdint.h>
 #include <string.h>
 
-#define BMP280_ADDR             0x76
+#define DevBMP280_ADDR                  0x76
 
-#define SLEEP_MODE              0x00
-#define FORCED_MODE             0x01
-#define NORMAL_MODE             0x03
+#define DevBMP280_SLEEP_MODE            0x00
+#define DevBMP280_FORCED_MODE           0x01
+#define DevBMP280_NORMAL_MODE           0x03
 
-#define STDBY_HALF_MS           0x00
-#define STDBY_62_AND_HALF_MS    0x01
-#define STDBY_125_MS            0x02
-#define STDBY_250_MS            0x03
-#define STDBY_500_MS            0x04
-#define STDBY_1_SEC             0x05
-#define STDBY_2_SEC             0x06
-#define STDBY_4_SEC             0x07
+#define DevBMP280_STDBY_HALF_MS         0x00
+#define DevBMP280_STDBY_62_AND_HALF_MS  0x01
+#define DevBMP280_STDBY_125_MS          0x02
+#define DevBMP280_STDBY_250_MS          0x03
+#define DevBMP280_STDBY_500_MS          0x04
+#define DevBMP280_STDBY_1_SEC           0x05
+#define DevBMP280_STDBY_2_SEC           0x06
+#define DevBMP280_STDBY_4_SEC           0x07
 
-#define TEMP_STOP								0x00
-#define TEMP_16_BIT							0x01
-#define TEMP_17_BIT							0x02
-#define TEMP_18_BIT							0x03
-#define TEMP_19_BIT							0x04
-#define TEMP_20_BIT							0x05
+#define DevBMP280_TEMP_STOP             0x00
+#define DevBMP280_TEMP_16_BIT           0x01
+#define DevBMP280_TEMP_17_BIT           0x02
+#define DevBMP280_TEMP_18_BIT           0x03
+#define DevBMP280_TEMP_19_BIT           0x04
+#define DevBMP280_TEMP_20_BIT           0x05
 
-#define PRESS_STOP							0x00
-#define PRESS_16_BIT						0x01
-#define PRESS_17_BIT						0x02
-#define PRESS_18_BIT						0x03
-#define PRESS_19_BIT						0x04
-#define PRESS_20_BIT						0x05
+#define DevBMP280_PRESS_STOP            0x00
+#define DevBMP280_PRESS_16_BIT          0x01
+#define DevBMP280_PRESS_17_BIT          0x02
+#define DevBMP280_PRESS_18_BIT          0x03
+#define DevBMP280_PRESS_19_BIT          0x04
+#define DevBMP280_PRESS_20_BIT          0x05
 
-#define POWER_MODE_MASK					0xFC
-#define STDBY_TIME_MASK					0x1F
-#define TEMP_MASK								0x1F
-#define PRESS_MASK							0xE3
+#define DevBMP280_POWER_MODE_MASK       0xFC
+#define DevBMP280_STDBY_TIME_MASK       0x1F
+#define DevBMP280_TEMP_MASK             0x1F
+#define DevBMP280_PRESS_MASK            0xE3
 
 typedef enum
 {
