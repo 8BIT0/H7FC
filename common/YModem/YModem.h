eb2e7bb9b430fa27b813bd94a5828703456c86e6
root <971168718@qq.com>
2024-04-12 18:18:43 +0800

    update

--- common/YModem/YModem.h
+++ common/YModem/YModem.h
@@ -5,19 +5,37 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define YMODEM_MAX_PROTO_SIZE 1024
+
 typedef enum
 {
     YModem_State_Idle = 0,
+    YModem_State_Tx,
+    YModem_State_Rx,
+    YModem_State_TimeOut,
 } YModem_State_List;
+
+typedef uint32_t (*YModem_Get_SysTick)(void);
+
+typedef struct
+{
+    uint32_t total_size;
+    uint32_t cur_size;
+    uint8_t *p_buf;
+} YModem_Stream_TypeDef;
 
 typedef struct
 {
     YModem_State_List state;
+    uint32_t timeout_threshold;
+    YModem_Get_SysTick sys_tick;
+    YModem_Stream_TypeDef tx_stream;
+    YModem_Stream_TypeDef rx_stream;
 } YModemObj_TypeDef;
 
 typedef struct
 {
-    bool (*Recv)(YModemObj_TypeDef *obj, uint8_t *p_data, uint16_t len);
+    void (*recv)(YModemObj_TypeDef *obj, uint8_t *p_buf, uint16_t len);
 } YModem_TypeDef;
 
 extern YModem_TypeDef YModem;
