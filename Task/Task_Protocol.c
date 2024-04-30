6d241aff5ea5b283bc71ca91cacfce81f1a7814c
root <971168718@qq.com>
2024-04-30 11:41:56 +0800

    update

--- Task/Task_Protocol.c
+++ Task/Task_Protocol.c
@@ -682,8 +682,7 @@ static void TaskFrameCTL_PortFrameOut_Process(void)
             SrvComProto.mav_msg_stream(&RadioProto_MAV_RcChannel, &MavStream, proto_arg, (ComProto_Callback)TaskFrameCTL_MavMsg_Trans);
             SrvComProto.mav_msg_stream(&RadioProto_MAV_Altitude,  &MavStream, proto_arg, (ComProto_Callback)TaskFrameCTL_MavMsg_Trans);
             
-            /* Proto mavlink message
-            ge through default port */
+            /* Proto mavlink message through default port */
             proto_monitor.port_type = Port_USB;
             proto_monitor.port_addr = USB_VCP_Addr;
             SrvComProto.mav_msg_stream(&TaskProto_MAV_RawIMU,       &MavStream, proto_arg, (ComProto_Callback)TaskFrameCTL_MavMsg_Trans);
