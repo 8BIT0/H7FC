9558def5b4e428143586a85d34315a729ca30213
root <971168718@qq.com>
2024-05-10 18:34:24 +0800

    update

--- Task/Task_Control.c
+++ Task/Task_Control.c
@@ -100,10 +100,8 @@ void TaskControl_Init(uint32_t period)
         /* parameter matched */
         TaskControl_Monitor.actuator_store_item = search_out.item;
         if (Storage.get(External_Flash, Para_User, search_out.item, &TaskControl_Monitor.actuator_setting, sizeof(SrvActuatot_Setting_TypeDef)) != Storage_Error_None)
-        {
             /* use defaule actuator setting */
             use_default = true;
-        }
     }
 
     if (use_default)
