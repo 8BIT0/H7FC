b504a90829f539370009ac53b6745fc4423fe9dd
root <971168718@qq.com>
2024-07-12 18:51:06 +0800

    all blackbox service update

--- Service/Srv_BlackBox_Card.c
+++ Service/Srv_BlackBox_Card.c
@@ -3,12 +3,14 @@
 /* external function */
 static bool SrvCard_BlackBox_Init(SrvBlackBox_Log_Callback callback);
 static bool SrvCard_BlackBox_PushData(uint8_t *p_data, uint16_t len);
+static bool SrvCard_BlackBox_Read(uint32_t addr_offset, uint8_t *p_data, uint16_t len);
 static bool SrvCard_BlackBox_Enable(void);
 static bool SrvCard_BlackBox_Disable(void);
 
 SrvBlackBox_TypeDef SrvCard_BlackBox = {
-    .init =  SrvCard_BlackBox_Init,
-    .push =  SrvCard_BlackBox_PushData,
+    .init = SrvCard_BlackBox_Init,
+    .push = SrvCard_BlackBox_PushData,
+    .read = SrvCard_BlackBox_Read,
     .enable = SrvCard_BlackBox_Enable,
     .disable = SrvCard_BlackBox_Disable,
 };
@@ -19,6 +21,11 @@ static bool SrvCard_BlackBox_Init(SrvBlackBox_Log_Callback callback)
 }
 
 static bool SrvCard_BlackBox_PushData(uint8_t *p_data, uint16_t len)
+{
+    return false;
+}
+
+static bool SrvCard_BlackBox_Read(uint32_t addr_offset, uint8_t *p_data, uint16_t len)
 {
     return false;
 }
--- Service/Srv_BlackBox_Com.c
+++ Service/Srv_BlackBox_Com.c
@@ -5,12 +5,14 @@
 /* external function */
 static bool SrvCom_BlackBox_Init(SrvBlackBox_Log_Callback callback);
 static bool SrvCom_BlackBox_PushData(uint8_t *p_data, uint16_t len);
+static bool SrvCom_BlackBox_Read(uint32_t addr_offset, uint8_t *p_data, uint16_t len);
 static bool SrvCom_BlackBox_Enable(void);
 static bool SrvCom_BlackBox_Disable(void);
 
 SrvBlackBox_TypeDef SrvCom_BlackBox = {
     .init = SrvCom_BlackBox_Init,
     .push = SrvCom_BlackBox_PushData,
+    .read = SrvCom_BlackBox_Read,
     .enable = SrvCom_BlackBox_Enable,
     .disable = SrvCom_BlackBox_Disable,
 };
@@ -21,6 +23,11 @@ static bool SrvCom_BlackBox_Init(SrvBlackBox_Log_Callback callback)
 }
 
 static bool SrvCom_BlackBox_PushData(uint8_t *p_data, uint16_t len)
+{
+    return false;
+}
+
+static bool SrvCom_BlackBox_Read(uint32_t addr_offset, uint8_t *p_data, uint16_t len)
 {
     return false;
 }
