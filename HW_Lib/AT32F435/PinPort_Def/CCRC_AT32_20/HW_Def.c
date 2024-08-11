6f8bf28b49aed3c90aa90e75b95eebc75b637623
8_BIT0 <971168718@qq.com>
2024-08-11 21:50:29 +0800

    CCRC/HW_Def update

--- HW_Lib/AT32F435/PinPort_Def/CCRC_AT32_20/HW_Def.c
+++ HW_Lib/AT32F435/PinPort_Def/CCRC_AT32_20/HW_Def.c
@@ -1,4 +1,53 @@
 #include "HW_Def.h"
 
+// MOTOR 1 B06
+// MOTOR 2 B07
+// MOTOR 3 A03
+// MOTOR 4 B01
 
+// SERIAL_TX 1 A09
+// SERIAL_TX 2 A02
+// SERIAL_TX 3 B11
+// SERIAL_TX 5 B09
+// SERIAL_TX 7 B04
 
+// SERIAL_RX 1 A10
+// SERIAL_RX 2 B00
+// SERIAL_RX 3 B10
+// SERIAL_RX 5 B08
+// SERIAL_RX 7 B03
+
+// I2C_SCL 2 H02
+// I2C_SDA 2 H03
+
+// LED 1 C13
+// LED 2 C14
+
+// SPI_SCK 1 A05
+// SPI_MISO 1 A06
+// SPI_MOSI 1 A07
+// SPI_SCK 2 B13
+// SPI_MISO 2 B14
+// SPI_MOSI 2 B15
+
+// FLASH_CS 1 B05
+// OSD_CS 1 B12
+// GYRO_EXTI 1 A15
+// GYRO_CS 1 A04
+
+// ADC_BATT 1 A00
+// ADC_CURR 1 A01
+
+// timer A08 AF1
+// # pin A08: TIM1 CH1 (AF1)
+// timer B06 AF2
+// # pin B06: TIM4 CH1 (AF2)
+// timer B07 AF2
+// # pin B07: TIM4 CH2 (AF2)
+// timer A03 AF1
+// # pin A03: TIM2 CH4 (AF1)
+// timer B01 AF2
+// # pin B01: TIM3 CH4 (AF2)
+
+// dma ADC 1 11
+// # ADC 1: DMA2 Channel 0 Channel 5
--- HW_Lib/AT32F435/PinPort_Def/CCRC_AT32_20/HW_Def.h
+++ HW_Lib/AT32F435/PinPort_Def/CCRC_AT32_20/HW_Def.h
@@ -5,10 +5,19 @@
 extern "C" {
 #endif
 
-#include <stddef.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include "Bsp_GPIO.h"
+#include "Bsp_DMA.h"
+#include "../../AT32F435/bsp/Bsp_Uart.h"
+#include "Bsp_IIC.h"
+#include "Bsp_SPI.h"
+#include "Bsp_Flash.h"
+#include "Dev_Led.h"
+#include "debug_util.h"
+#include "util.h"
+#include "../../common/gen_physic_def/imu_data.h"
+
+/* device support */
+#include "Dev_W25Qxx.h"
 
 #ifdef __cplusplus
 }
