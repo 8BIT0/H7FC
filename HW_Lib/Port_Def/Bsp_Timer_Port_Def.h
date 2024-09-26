006a0491359dd6d4be97ab932becfb6c1ee2df50
root <971168718@qq.com>
2024-09-26 09:56:02 +0800

    update

--- HW_Lib/Port_Def/Bsp_Timer_Port_Def.h
+++ HW_Lib/Port_Def/Bsp_Timer_Port_Def.h
@@ -85,6 +85,7 @@ typedef struct
     bool (*de_init)(BspTimerPWMObj_TypeDef *obj);
     void (*set_dma_pwm)(BspTimerPWMObj_TypeDef *obj);
     void (*dma_trans)(BspTimerPWMObj_TypeDef *obj);
+    void (*pwm_trans)(BspTimerPWMObj_TypeDef *obj);
     uint32_t (*get_clock_freq)(BspTimerPWMObj_TypeDef *obj);
 } BspTimerPWM_TypeDef;
 
