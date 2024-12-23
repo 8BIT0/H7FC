d043d43339017d8f40f4f64d18478de1af3f6af4
root <971168718@qq.com>
2024-12-23 17:47:57 +0800

    math_util update

--- Algorithm/math_util.h
+++ Algorithm/math_util.h
@@ -7,6 +7,15 @@ extern "C" {
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+/* Cbn matrix definition */
+typedef union
+{
+    float buf[9];
+    float matrix[3][3];
+} M_Cbn_TypeDef;
 
 inline float Deg2Rad(float deg)
 {
@@ -22,6 +31,27 @@ inline float Rad2Deg(float rad)
 inline float g2Mpss(float g)
 {
     return g * 9.8;
+}
+
+inline M_Cbn_TypeDef AttConvert2Cbn(float pitch, float roll, float yaw)
+{
+    M_Cbn_TypeDef tmp;
+
+    memset(&tmp, 0, sizeof(M_Cbn_TypeDef));
+
+    tmp.matrix[0][0] = cos(yaw) * cos(pitch);
+    tmp.matrix[0][1] = cos(pitch) * sin(yaw);
+    tmp.matrix[0][2] = -sin(pitch);
+    
+    tmp.matrix[1][0] = cos(yaw) * sin(pitch) *sin(roll) - cos(roll) * sin(yaw);
+    tmp.matrix[1][1] = cos(yaw) * cos(roll) + sin(yaw) * sin(roll) * sin(pitch);
+    tmp.matrix[1][2] = cos(pitch) * sin(roll);
+
+    tmp.matrix[2][0] = sin(yaw) * sin(roll) + cos(roll) * cos(yaw) * sin(pitch);
+    tmp.matrix[2][1] = cos(roll) * sin(yaw) * sin(pitch) - cos(yaw) * sin(roll);
+    tmp.matrix[2][2] = cos(pitch) * cos(roll);
+    
+    return tmp;
 }
 
 #ifdef __cplusplus
