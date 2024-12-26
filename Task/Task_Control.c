e375853eefb0c22c336bfe953352010975fb8622
8_BIT0 <971168718@qq.com>
2024-12-26 23:27:52 +0800

    update

--- Task/Task_Control.c
+++ Task/Task_Control.c
@@ -286,8 +286,8 @@ static void TaskControl_FlightControl_Polling(ControlData_TypeDef *exp_ctl_val, 
         {
             att_ctl_exp.pitch  = TaskControl_Convert_CtlData(exp_ctl_val->pitch_percent, TaskControl_Monitor.ctl_para.pitch_range, TaskControl_Monitor.ctl_para.att_rate);
             att_ctl_exp.roll   = TaskControl_Convert_CtlData(exp_ctl_val->roll_percent,  TaskControl_Monitor.ctl_para.roll_range,  TaskControl_Monitor.ctl_para.att_rate);
-            att_ctl_exp.gyro_x = TaskControl_Convert_CtlData(exp_ctl_val->roll_percent, TaskControl_Monitor.ctl_para.gx_range,    TaskControl_Monitor.ctl_para.gx_rate);
-            att_ctl_exp.gyro_y = TaskControl_Convert_CtlData(exp_ctl_val->pitch_percent,  TaskControl_Monitor.ctl_para.gy_range,    TaskControl_Monitor.ctl_para.gy_rate);
+            att_ctl_exp.gyro_x = TaskControl_Convert_CtlData(exp_ctl_val->roll_percent,  TaskControl_Monitor.ctl_para.gx_range,    TaskControl_Monitor.ctl_para.gx_rate);
+            att_ctl_exp.gyro_y = TaskControl_Convert_CtlData(exp_ctl_val->pitch_percent, TaskControl_Monitor.ctl_para.gy_range,    TaskControl_Monitor.ctl_para.gy_rate);
             att_ctl_exp.gyro_z = TaskControl_Convert_CtlData(exp_ctl_val->yaw_percent,   TaskControl_Monitor.ctl_para.gz_range,    TaskControl_Monitor.ctl_para.gz_rate);
 
             if (exp_ctl_val->control_mode == AngularSpeed_Control)
